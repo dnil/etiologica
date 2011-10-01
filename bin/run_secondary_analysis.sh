67fed98e2442b3cb4a9d4b204c7d5cf16e46ec72
Daniel Nilsson <daniel.nilsson@scilifelab.se>
2011-10-01 21:59:46 +0200

    More extensive automagic variant filtering. Optionally put Mosaik dat files etc on local node scratch.

--- bin/run_secondary_analysis.sh
+++ bin/run_secondary_analysis.sh
@@ -4,8 +4,6 @@
 # Released under the Perl Artistic License.
 #
 # Documentation available in this file. Use e.g. perldoc to view, or pod2man to create a manual.
-
-# . etiologica_site_config.sh
 #
 # TODO: decide on parameter config structures 
 #
@@ -58,7 +56,7 @@ The pipeline adheres to environment exported variables.
 
 =over 4
 
-=item C<SCRATCH> I<path>
+=item C<SNIC_TMP> I<path>
 
 Designated temp area. Please make it local and sufficiently big for the sake of MOSAIK.
 Can be overruled by directly setting TMP or MOSAIK_TMP, with the latter having the highest precedence rank.
@@ -120,12 +118,6 @@ then
 fi
 log "BCFTOOLS: $BCFTOOLS" "main"
 
-if [ -z "$VCFUTILS" ]
-then
-    VCFUTILS=/home/daniel/src/samtools-0.1.17/bcftools/vcfutils.pl
-fi
-log "VCFUTILS: $VCFUTILS" "main"
-
 if [ -z "$FASTQC" ]
 then 
     FASTQC=/home/daniel/src/FastQC/fastqc
@@ -136,7 +128,7 @@ if [ -z "$ANNOVARBIN" ]
 then
     ANNOVARBIN=/home/daniel/src/annovar
 fi
-log "SAMTOOLS: $SAMTOOLS" "main"
+log "ANNOVARBIN: $ANNOVARBIN" "main"
 
 if [ -z "$AVDBDIR" ]
 then
@@ -144,12 +136,24 @@ then
 fi
 log "ANNOVAR AVDBDIR: $AVDBDIR" "main"
 
-if [ -z "$ANNOVAR_1KG_MAFR" ]
+if [ -z "$ANNOVAR_DISPENSABLE" ]
+then
+    ANNOVAR_DISPENSABLE=/home/daniel/src/annovar/example/dispensable.all
+fi
+log "ANNOVAR AVDBDIR: $AVDBDIR" "main"
+
+
+if [ -z "$ANNOVAR_1KG_MAF" ]
 then
     ANNOVAR_1KG_MAF=0.1
 fi
-log "ANNOVAR 1KG_MAF: $ANNOVAR_1KG_MAF" "main"
+log "ANNOVAR 1KG MAF: $ANNOVAR_1KG_MAF" "main"
 
+if [ -z "$ANNOVAR_PP2_BENIGN" ]
+then
+    ANNOVAR_PP2_BENIGN=0.85
+fi
+log "ANNOVAR PP2 BENIGN: $ANNOVAR_PP2_BENIGN" "main"
 
 if [ -z "$TMP" ] 
 then
@@ -175,6 +179,12 @@ then
     fi
 fi
 log "MOSAIKBIN: $MOSAIKBIN" "main"
+
+if [ -z "$MOSAIK_DAT_ON_SCRATCH" ]
+then
+    MOSAIK_DAT_ON_SCRATCH="no"
+fi
+log "MOSAIK_DAT_ON_SCRATCH: $MOSAIK_DAT_ON_SCRATCH" "main"
 
 if [ -z "$MOSAIK_mismatches" ]
 then 
@@ -229,7 +239,13 @@ then
 fi
 log "MOSAIK - MOSAIK_CORES: $MOSAIK_CORES" "main"
 
-# VCFTOOLS
+# VCFUTILS (from samtools/bcftools)
+if [ -z "$VCFUTILS" ]
+then
+    VCFUTILS=/home/daniel/src/samtools-0.1.17/bcftools/vcfutils.pl
+fi
+log "VCFUTILS: $VCFUTILS" "main"
+
 
 if [ -z "$VCFUTILS_min_call_cov" ]
 then
@@ -268,12 +284,12 @@ reference_jump=${reference_dat%%.dat}.$MOSAIK_mjump
 if needsUpdate $reference_dat $REFERENCE $MOSAIKBIN
 then
     runme="$MOSAIKBIN/MosaikBuild -fr $REFERENCE -oa $reference_dat"
-    vanillaRun "$runme" "$reference_dat" "temp" "MosaikBuild"
+    vanillaRun "$runme" "$reference_dat" "common" "MosaikBuild"
 
     if [ "$JUMP" == "yes" ]
     then
 	runme="$MOSAIKBIN/MosaikJump -ia $reference_dat -hs $MOSAIK_mjump -out $reference_jump -mhp $MOSAIK_mhp"
-	vanillaRun "$runme" "$reference_jump" "temp" "MosaikJump"
+	vanillaRun "$runme" "$reference_jump" "common" "MosaikJump"
     fi
 fi
 
@@ -387,42 +403,62 @@ do
 	# trim
 
 	patient_dat=${patient_fastq_gz%%fastq.gz}dat
-	if [ "$MATEPAIRS" == "0" ]
-	then
-	    if needsUpdate $patient_dat $patient_fastq_gz
-	    then
-		runme="$MOSAIKBIN/MosaikBuild -q $patient_fastq_gz -out $patient_dat -st illumina"
-		vanillaRun "$runme" "$patient_dat" "temp" "MosaikBuild"
-	    fi
-	else
-	    patient_2_fastq_gz=${patient_fastq_gz%%_1.fastq.gz}_2.fastq.gz
-	    if needsUpdate $patient_dat $patient_fastq_gz $patient_2_fastq_gz
-	    then
-		runme="$MOSAIKBIN/MosaikBuild -q $patient_fastq_gz -q2 $patient_2_fastq_gz -out $patient_dat -st illumina"
-		vanillaRun "$runme" "$patient_dat" "temp" "MosaikBuild"
-	    fi
-	fi
 
-	MOSAIK_ALIGN_MODE=all
-	if [ "$MATEPAIRS" == "0" ]
+	# If mosaik_dat_on_scratch is enabled, we only really want to redo all the dat files in case the final product mosaik bam is outdated. Yay, let us reinvent make!
+	run_align="yes"
+	if [ $MOSAIK_DAT_ON_SCRATCH == "yes" ]
 	then
-	    MOSAIK_ALIGN_MODE=unique
-	    dupstring=""
-	fi
-       
-	patient_aln_dat=${patient_dat%%dat}mosaik.dat
-	if needsUpdate $patient_aln_dat $patient_dat $reference_jump $MOSAIKBIN/MosaikAligner
-	then
-	    if [ "$JUMP" == "yes" ]
+	    mkdir -p $MOSAIK_TMP/`dirname $patient_dat`
+	    patient_dat=$MOSAIK_TMP/$patient_dat
+
+	    final_patient_bam=${patient_dat%%dat}.mosaik.bam
+	    if needsUpdate ${final_patient_bam} ${patient_fastq_gz} $MOSAIKBIN/MosaikBuild $MOSAIKBIN/MosaikAligner $MOSAIKBIN/MosaikText $reference_jump $MOSAIKBIN/MosaikDupSnoop $MOSAIKBIN/MosaikSort
 	    then
-		runme="$MOSAIKBIN/MosaikAligner -in $patient_dat -ia $reference_dat -out $patient_aln_dat -m $MOSAIK_ALIGN_MODE -hs $MOSAIK_mjump -bw $MOSAIK_sw_bandwidth -j $reference_jump -mhp $MOSAIK_mhp -mm $MOSAIK_mismatches -act $MOSAIK_clustersize -p $MOSAIK_CORES"
+		run_align="yes"
 	    else
-		runme="$MOSAIKBIN/MosaikAligner -in $patient_dat -ia $reference_dat -out $patient_aln_dat -m $MOSAIK_ALIGN_MODE -bw $MOSAIK_sw_bandwidth -mm $MOSAIK_mismatches -act $MOSAIK_clustersize -p $MOSAIK_CORES"
+		run_align="no"
 	    fi
-	    vanillaRun "$runme" "$patient_aln_dat" "temp" "MosaikAligner"
 	fi
 
-	: <<POD_MOSAIKDUP
+	if [ $run_align == "yes" ] 
+	then
+	    if [ "$MATEPAIRS" == "0" ]
+	    then
+		if needsUpdate $patient_dat $patient_fastq_gz
+		then
+		    runme="$MOSAIKBIN/MosaikBuild -q $patient_fastq_gz -out $patient_dat -st illumina"
+		    vanillaRun "$runme" "$patient_dat" "temp" "MosaikBuild"
+		fi
+	    else
+		patient_2_fastq_gz=${patient_fastq_gz%%_1.fastq.gz}_2.fastq.gz
+		if needsUpdate $patient_dat $patient_fastq_gz $patient_2_fastq_gz
+		then
+		    runme="$MOSAIKBIN/MosaikBuild -q $patient_fastq_gz -q2 $patient_2_fastq_gz -out $patient_dat -st illumina"
+		    vanillaRun "$runme" "$patient_dat" "temp" "MosaikBuild"
+		fi
+	    fi
+
+	    MOSAIK_ALIGN_MODE=all
+	    if [ "$MATEPAIRS" == "0" ]
+	    then
+		MOSAIK_ALIGN_MODE=unique
+		dupstring=""
+	    fi
+	    
+	    patient_aln_dat=${patient_dat%%dat}mosaik.dat
+
+	    if needsUpdate $patient_aln_dat $patient_dat $reference_jump $MOSAIKBIN/MosaikAligner
+	    then
+		if [ "$JUMP" == "yes" ]
+		then
+		    runme="$MOSAIKBIN/MosaikAligner -in $patient_dat -ia $reference_dat -out $patient_aln_dat -m $MOSAIK_ALIGN_MODE -hs $MOSAIK_mjump -bw $MOSAIK_sw_bandwidth -j $reference_jump -mhp $MOSAIK_mhp -mm $MOSAIK_mismatches -act $MOSAIK_clustersize -p $MOSAIK_CORES"
+		else
+		    runme="$MOSAIKBIN/MosaikAligner -in $patient_dat -ia $reference_dat -out $patient_aln_dat -m $MOSAIK_ALIGN_MODE -bw $MOSAIK_sw_bandwidth -mm $MOSAIK_mismatches -act $MOSAIK_clustersize -p $MOSAIK_CORES"
+		fi
+		vanillaRun "$runme" "$patient_aln_dat" "temp" "MosaikAligner"
+	    fi
+
+	    : <<POD_MOSAIKDUP
 
 =head1 DEAL WITH DUPLICATES
 
@@ -434,42 +470,51 @@ duplicate checking is turned off.
 
 POD_MOSAIKDUP
 
-	patient_lib_dupdata_dir=${patient_aln_dat%%.mosaik.dat}_DupData
-	if [ "$MATEPAIRS" != 0 ] 
-	then
-	    if needsUpdate ${patient_lib_dupdata_dir}/.db $patient_aln_dat $MOSAIKBIN/MosaikDupSnoop
+	    patient_lib_dupdata_dir=${patient_aln_dat%%.mosaik.dat}_DupData
+
+	    if [ "$MATEPAIRS" != 0 ] 
 	    then
-		if [ ! -d $patient_lib_dupdata_dir ]
+		if needsUpdate ${patient_lib_dupdata_dir}/.db $patient_aln_dat $MOSAIKBIN/MosaikDupSnoop
 		then
-		    mkdir $patient_lib_dupdata_dir
+		    if [ ! -d $patient_lib_dupdata_dir ]
+		    then
+			mkdir $patient_lib_dupdata_dir
+		    fi
+		    
+		    runme="$MOSAIKBIN/MosaikDupSnoop -in $patient_aln_dat -od $patient_lib_dupdata_dir"
+		    vanillaRun "$runme" "$patient_lib_dupdata_dir/.db" "temp" "MosaikDupSnoop"
+		fi
+	    fi
+
+	    patient_sorted=${patient_aln_dat%%dat}sorted.dat
+
+	    if needsUpdate ${patient_sorted} $patient_aln_dat $MOSAIKBIN/MosaikSort
+	    then
+		if [ "$MATEPAIRS" == 0 ]
+		then 
+		    dupstring=""
+		else
+		    dupstring="-dup $patient_lib_dupdata_dir"
 		fi
 		
-		runme="$MOSAIKBIN/MosaikDupSnoop -in $patient_aln_dat -od $patient_lib_dupdata_dir"
-		vanillaRun "$runme" "$patient_lib_dupdata_dir/.db" "temp" "MosaikDupSnoop"
+		runme="$MOSAIKBIN/MosaikSort -in $patient_aln_dat -out $patient_sorted $dupstring"
+		vanillaRun "$runme" "$patient_sorted" "temp" "MosaikSort"
 	    fi
-	fi
-
-	patient_sorted=${patient_aln_dat%%dat}sorted.dat
-	if needsUpdate ${patient_sorted} $patient_aln_dat $MOSAIKBIN/MosaikSort
-	then
-	    if [ "$MATEPAIRS" == 0 ]
-	    then 
-		dupstring=""
-	    else
-		dupstring="-dup $patient_lib_dupdata_dir"
-	    fi
-	    
-	    runme="$MOSAIKBIN/MosaikSort -in $patient_aln_dat -out $patient_sorted $dupstring"
-	    vanillaRun "$runme" "$patient_sorted" "temp" "MosaikSort"
-	fi
 
        # TODO: mosaik merge, when several libs for one patient are available.
 
-	patient_bam=${patient_sorted%%sorted.dat}bam
-	if needsUpdate ${patient_bam} ${patient_sorted} $MOSAIKBIN/MosaikText
-	then
-	    runme="$MOSAIKBIN/MosaikText -in $patient_sorted -bam $patient_bam"
-	    vanillaRun "$runme" "$patient_bam" "result" "MosaikText -bam"
+	    patient_bam=${patient_sorted%%sorted.dat}bam
+
+	    if [ $MOSAIK_DAT_ON_SCRATCH == "yes" ] 
+	    then
+		patient_bam=${patient_bam##$MOSAIK_TMP}
+	    fi
+
+	    if needsUpdate ${patient_bam} ${patient_sorted} $MOSAIKBIN/MosaikText
+	    then
+		runme="$MOSAIKBIN/MosaikText -in $patient_sorted -bam $patient_bam"
+		vanillaRun "$runme" "$patient_bam" "result" "MosaikText -bam"
+	    fi
 	fi
 
 	patient_bcf=${patient_fastq_gz%%fastq.gz}var.raw.bcf
@@ -538,24 +583,72 @@ POD_MOSAIKDUP
 	    vanillaRun "$runme" "$patient_maf_exonic_variant" "result" "ANNOVAR 1000g MAF $ANNOVAR_1KG_MAF --geneanno"
 	fi
 
+	patient_pp2_filter=${patient_maf_filter}.hg19_ljb_pp2_filtered
+	if needsUpdate $patient_pp2_filter $patient_maf_filter $ANNOVARBIN/annotate_variation.pl
+	then
+	    runme="$ANNOVARBIN/annotate_variation.pl --filter --dbtype ljb_pp2 --reverse --score_threshold $ANNOVAR_PP2_BENIGN --buildver hg19 $patient_pass_avlist $AVDBDIR"
+	    vanillaRun "$runme" "$patient_pp2_filter" "result" "ANNOVAR --filter PolyPhen2 benign level $ANNOVAR_PP2_BENIGN"
+	fi
+
+	patient_pp2_exonic_variant=${patient_pp2_filter}.exonic_variant_function
+	if needsUpdate $patient_pp2_exonic_variant $patient_pp2_filter $ANNOVARBIN/annotate_variation.pl
+	then
+	    runme="$ANNOVARBIN/annotate_variation.pl --buildver hg19 $patient_pp2_filter $AVDBDIR"
+	    vanillaRun "$runme" "$patient_pp2_exonic_variant" "result" "ANNOVAR PolyPhen2 benign $ANNOVAR_PP2_BENIGN --geneanno"
+	fi
+	
+	patient_pp2_variant_function=${patient_pp2_filter}.variant_function
+	patient_splicing=${patient_pp2_variant_function}.splicing
+	if needsUpdate $patient_splicing $patient_pp2_variant_function
+	then
+	    runme="awk '(\$1 == \"exonic;splicing\" || \$1 == \"splicing\") { print }' $patient_pp2_variant_function > $patient_splicing"
+	    vanillaRun "$runme" "$patient_splicing" "result" "Extract splicing variants."
+	fi	    
+
+        # gather exonic;splicing from variant_function
+	patient_all_variant_function=${patient_pass_avlist}.variant_function
+	patient_all_splicing=${patient_all_variant_function}.splicing
+	if needsUpdate $patient_all_splicing $patient_all_variant_function
+	then
+	    runme="awk '(\$1 == \"exonic;splicing\" || \$1 == \"splicing\") { print }' $patient_all_variant_function > $patient_all_splicing"
+	    vanillaRun "$runme" "$patient_all_splicing" "result" "Extract passed splicing variants."
+	fi
+
+	patient_not_syn=${patient_pp2_exonic_variant}.not_syn
+	if needsUpdate $patient_not_syn $patient_pp2_exonic_variant
+	then
+	    runme="cut -f2- $patient_pp2_exonic_variant | awk '(\$1 \!= \"synonymous\") { print }' > $patient_not_syn"
+	    vanillaRun "$runme" "$patient_not_syn" "result" "Ignore synonymous variants for now."
+	fi
+
+	# hom | 2xhet (remember splicing!)	
+	patient_recessive_genelist=${patient_pp2_variant_function}.recessive_model
+	if needsUpdate $patient_recessive_genelist $patient_not_syn $patient_splicing
+	then
+	    cat ${patient_not_syn} $patient_splicing |perl -e'while(<STDIN>) { chomp; @r=split(/\t/); @transcripts=split(/[:;(]+/,$r[1]); $gene=$transcripts[0]; $gene_row=$gene."\t".join("\t",@r)."\n"; $nvars{$gene}++; $vars{$gene}.=$gene_row; if(($r[7] eq "hom") || ($r[7] eq "het" && $nvars{$gene} >1) ) { $modelok{$gene}=1; } } foreach $gene (keys %modelok) {print $vars{$gene}};' > $patient_recessive_genelist 
+	fi
+
+	patient_indisp=${patient_recessive_genelist}.indisp
+	if needsUpdate $patient_indisp $patient_recessive_genelist
+	then
+	  runme="grep -v -w -f $ANNOVAR_DISPENSABLE $patient_recessive_genelist > $patient_indisp"
+	fi
+
+# sift
 #	patient_sift_filter=${patient_maf_filter}.hg19_ALL.sites.2010_11_filtered
 #	if needsUpdate $patient_sift_filter $patient_avlistOA $ANNOVARBIN/annotate_variation.pl
 #	then
 #	    runme="$ANNOVARBIN/annotate_variation.pl --filter --dbtype avsift --buildver hg19 $patient_maf_filter $AVDBDIR"
 #	    vanillaRun "$runme" "$patient_maf_filter" "result" "ANNOVAR --filter 1000g maf $ANNOVAR_1KG_MAF"
 #	fi
+# structural variants
 #	    $ANNOVARBIN/annotate_variation.pl --regionanno --buildver hg19 --dbtype dgv $patient_avlist $AVDBDIR
+# to csv, many sources --- slow!
 #	    export PATH=$PATH:"$ANNOVARBIN"
 #	    $ANNOVARBIN/summarize_annovar.pl --buildver hg19 --verdbsnp 132 --outfile ${patient_fastq_gz%%.fastq.gz} $patient_avlist $AVDBDIR 
 #	    registerFile other_annovar_files temp
-
-# gather exonic;splicing from variant_function
-# gather indel from exonic_variant_function
-# gather nonsynonymous -> sift & polyphen2 -> keep.
-
-# log hom. for gene, if more than one het, log compound
-
-# cnv analysis
+#
+        # cnv analysis
 	
 	releaseLock $patient_fastq_gz
 	log "Workblock exit: released lock on $patient_fastq_gz." "main"
